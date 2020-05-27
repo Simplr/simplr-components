bcbb9958a8ec1e88a3768e288b8f9b16ac0e1d1f
Matsuuu <huhta.matias@gmail.com>
2020-05-27 12:09:37 +0300

    Add helper functions to input base

---
+++ lib/input/base/simplr-input-base.d.ts
@@ -0,0 +1,8 @@
+export declare class SimplrInputBase {
+  checkValidity(): boolean;
+  getValue(clearInput?: boolean): string;
+  setValue(value: string): void;
+  clear(): void;
+}
+
+export default SimplrInputBase;
