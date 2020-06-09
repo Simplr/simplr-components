46a5939ed09b76d4cb8216e3299adc423bfe7ab5
Matsuuu <huhta.matias@gmail.com>
2020-06-09 11:51:10 +0300

    Add Lerna to project

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
 import { addParameters, setCustomElements } from '@open-wc/demoing-storybook';
 
+const _customElementsDefine = window.customElements.define;
+window.customElements.define = (name, cl, conf) => {
+  if (!customElements.get(name)) {
+    _customElementsDefine.call(window.customElements, name, cl, conf);
+  }
+};
+
 addParameters({
   docs: {
     iframeHeight: '200px',
